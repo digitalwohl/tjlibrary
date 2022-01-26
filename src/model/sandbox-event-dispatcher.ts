21e38845eed8b777e7662f962016783c2c655205
Dinu Sebastian Liviu <sebastian@wearewip.com>
2022-01-26 21:57:19 +0200

    fix 2 for event sending from to be jailed page

--- src/model/sandbox-event-dispatcher.ts
+++ src/model/sandbox-event-dispatcher.ts
@@ -4,6 +4,6 @@ export default class SandboxEventDispatcher {
     private PARENT_TARGET_ORIGIN = 'https://localhost/tabjail/sandbox';
 
     public sendSandboxEvent(sandboxEvent: SandboxEvent) {
-        window.parent.postMessage(JSON.stringify(sandboxEvent), this.PARENT_TARGET_ORIGIN);
+        window.parent.postMessage(sandboxEvent, this.PARENT_TARGET_ORIGIN);
     }
 }
