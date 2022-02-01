d36dbb37387d374709c315af85be0c75587575f2
Dinu Sebastian Liviu <sebastian@wearewip.com>
2022-02-01 12:27:53 +0200

    fixed missing import

--- dist/api/chrome/chrome.d.ts
+++ dist/api/chrome/chrome.d.ts
@@ -1,6 +1,8 @@
 import SandboxEvent from "../../model/sandbox-event";
+import Tabs from "./tabs/tabs";
 import Windows from "./windows/windows";
 export default class Chrome {
     windows: Windows;
+    tabs: Tabs;
     onEventFromSandbox(sandboxEvent: SandboxEvent): void;
 }
--- src/api/chrome/chrome.ts
+++ src/api/chrome/chrome.ts
@@ -1,9 +1,11 @@
 import SandboxEvent from "../../model/sandbox-event";
+import Tabs from "./tabs/tabs";
 import Windows from "./windows/windows";
 
 export default class Chrome {
 
     public windows = new Windows();
+    public tabs = new Tabs();
 
     onEventFromSandbox(sandboxEvent: SandboxEvent): void {
         this.windows.onEventFromSandbox(sandboxEvent);
