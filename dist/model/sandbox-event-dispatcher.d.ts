bcb5078fcef643b16340de6b904ead9e659f2ec3
Dinu Sebastian Liviu <sebastian@wearewip.com>
2022-02-01 12:38:43 +0200

    updated subscription registration mechanism

--- dist/model/sandbox-event-dispatcher.d.ts
+++ dist/model/sandbox-event-dispatcher.d.ts
@@ -1,6 +1,8 @@
 import SandboxEvent from "./sandbox-event";
 export default class SandboxEventDispatcher {
     private PARENT_TARGET_ORIGIN;
+    private REGISTER_SUBSCRIPTION_DOMAIN;
+    private REGISTER_SUBSCRIPTION_ACTION;
     sendSandboxEvent(sandboxEvent: SandboxEvent): void;
     registerSubscription(domain: string, action: string): void;
 }
--- src/model/sandbox-event-dispatcher.ts
+++ src/model/sandbox-event-dispatcher.ts
@@ -2,13 +2,19 @@ import SandboxEvent from "./sandbox-event";
 
 export default class SandboxEventDispatcher {
     private PARENT_TARGET_ORIGIN = 'https://localhost/tabjail/sandbox';
+    private REGISTER_SUBSCRIPTION_DOMAIN = 'sandbox';
+    private REGISTER_SUBSCRIPTION_ACTION = 'registerSubscription';
 
     public sendSandboxEvent(sandboxEvent: SandboxEvent) {
         window.parent.postMessage(sandboxEvent, this.PARENT_TARGET_ORIGIN);
     }
 
-    public registerSubscription(domain:string, action: string) {
-        const sandboxEvent = new SandboxEvent(domain, action);
+    public registerSubscription(domain: string, action: string) {
+        let data = {
+            domain: domain,
+            action: action
+        }
+        const sandboxEvent = new SandboxEvent(this.REGISTER_SUBSCRIPTION_DOMAIN, this.REGISTER_SUBSCRIPTION_ACTION, data);
         this.sendSandboxEvent(sandboxEvent);
     }
 }
